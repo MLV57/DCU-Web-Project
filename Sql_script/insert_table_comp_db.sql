da7b4b89bab4e277e14b35196c051c8ebe22c2de
daraghclarke <81627822+daraghclarke@users.noreply.github.com>
2021-04-25 17:16:09 +0100

    Add files via upload

---
+++ Sql_script/insert_table_comp_db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE competition_form_gogo
+(NAME VARCHAR(20) NOT NULL,
+AGE VARCHAR(10),
+GENDER VARCHAR(20),
+PHONE VARCHAR(20),
+EMAIL VARCHAR(20),
+DEST VARCHAR(50),
+ORIGIN VARCHAR(20),
+WHY VARCHAR(50))
+
+
+INSERT INTO competition_form_gogo VALUES
+('Daragh','23','male','+01222888','daragh@mail.com','Portugal','Dublin','Holiday');
