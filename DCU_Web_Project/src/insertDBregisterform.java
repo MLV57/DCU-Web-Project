630ac824904e7746269b9714fb1de1063349ac22
Your NameAdriK78 <adrien.kreutz@gmail.com>
2021-04-25 20:46:38 +0200

    Signup working

--- DCU_Web_Project/src/insertDBregisterform.java
+++ DCU_Web_Project/src/insertDBregisterform.java
@@ -1,13 +1,13 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,12 +16,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class insertDBregisterform
+ * Servlet implementation class CheckDBforLogin
  */
 @WebServlet("/insertDBregisterform")
 public class insertDBregisterform extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
+
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,55 +38,50 @@ public class insertDBregisterform extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-               
-       
-        //out.println("You are " + request.getParameter("age") + " years old!");
-        //out.println("</body></html>");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+doGet(request, response);
 		// TODO Auto-generated method stub
-		doGet(request, response);
 		Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
-        String JDBCUrl = "jdbc:mysql://localhost:3306";
-        String username = "root";
-        String password = "OnigiriSan";
-       // PrintWriter out = response.getWriter();
+        String JDBCUrl = "jdbc:mysql://ee417.crxkzf89o3fh.eu-west-1.rds.amazonaws.com:3306/testdb";
+        String username1 = "ee417";
+        String password1 = "ee417";
+        PrintWriter out = response.getWriter();
+
         try {
             System.out.println("\nConnecting to the SSD Database......");
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(JDBCUrl, username, password);
+            //connection to the database
+            con = DriverManager.getConnection(JDBCUrl, username1, password1);
         }
         catch (Exception e) {
             System.out.println("\nAn error has occurred during the connection phase!  This is most likely due to your CLASSPATH being set wrong and the"
                     + "  classes unable to be found.  Otherwise the database itself may be down.  Try telneting to port 3306 and see if it is up!");
             e.printStackTrace();
             System.exit(0);
-        }   
-        
+        }
 
-       String USname = request.getParameter("USname");
-       String name = request.getParameter("name");
-       String sname = request.getParameter("sname");
-       String Gender = request.getParameter("Gender");
-       String uname = request.getParameter("uname");
-       String Eid = request.getParameter("Eid");
-       String email = request.getParameter("email");
-       String phone = request.getParameter("phone");
-       String address = request.getParameter("address");
-       String pass = request.getParameter("pass");
-       
+
+       String USname = request.getParameter("fuser");
+       String name = request.getParameter("fname");
+       String sname = request.getParameter("lname");
+       String Gender = request.getParameter("fgender");
+       String uname = request.getParameter("funinersity");
+       String Eid = request.getParameter("fid");
+       String email = request.getParameter("femail");
+       String phone = request.getParameter("fphone");
+       String address = request.getParameter("fadd");
+       String pass = request.getParameter("fpass");
+
        //System.out.println(firstName);
 	 try {
-		  PreparedStatement pstmt = con.prepareStatement("INSERT INTO testdb.customers(Username,firstName,lastName,gender,UniversityName,EnrollmentID,ContactNumber,Address,email,Pwd) VALUES (?,?,?,?,?,?,?,?,?,?)");
+		  PreparedStatement pstmt = con.prepareStatement("INSERT INTO groupH_user(Username,firstName,lastName,gender,UniversityName,EnrollmentID,ContactNumber,Address,email,Pwd) VALUES (?,?,?,?,?,?,?,?,?,?)");
 				  pstmt.clearParameters();       // Clears any previous parameters
 				  pstmt.setString(1, USname);
 				  pstmt.setString(2, name);
@@ -97,7 +94,7 @@ public class insertDBregisterform extends HttpServlet {
 				  pstmt.setString(9, email);
 				  pstmt.setString(10, pass);
 				  pstmt.executeUpdate();
-				 
+
 
 		// System.out.println("\nConnection Successful..... creating statement....");
      	  //   stmt = con.createStatement();
@@ -117,16 +114,16 @@ public class insertDBregisterform extends HttpServlet {
         }
 
         finally {
-	     try {    
+	     try {
 	         if (rs != null) rs.close();
 		 if (stmt != null) stmt.close();
 		 if (con != null) con.close();
 	     }
 	     catch (Exception ex) {
-	         System.out.println("An error occurred while closing down connection/statement"); 
+	         System.out.println("An error occurred while closing down connection/statement");
             }
         }
-		
+
 	}
 
 }
